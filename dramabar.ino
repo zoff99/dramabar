a50ed13a03f51fb4b6e2c903945ef93a009b2557
LuPro <LuPro@users.noreply.github.com>
2016-08-01 17:50:39 +0200

    Added normalization to dramabar.ino & some more
    Since the dramabar tends to always be either "super-happy-rainbow" mood, or "alarming-blinking-red" I wrote normalization for the mood, which slowly sets the mood back to neutral (7) - one point an hour.
    
    I have outsourced mood shifting one up or down (better or worse mood) into a function and shortened the init_display() function

--- dramabar.ino
+++ dramabar.ino
@@ -2,7 +2,9 @@
 #include "SPI.h" // Comment out this line if using Trinket or Gemma
 
 
-
+unsigned long expTime = 3600000;    //Time in ms for mood expiration (eg: every X ms mood goes 1 point nearer to normal)
+unsigned long now = 0;              //Time in ms now (measured from boot)
+unsigned long t_prev = 0;           //Time in ms, from last run of loop
 
 int ledPin1 = 6;    // LED connected to digital pin 9
 int buttonPin1 = A5;
@@ -12,8 +14,8 @@ int buttonPin2 = A4;
 
 
 
-int currentpos=7;
-int lastpos=currentpos;
+int currentpos = 7;
+int lastpos = currentpos;
 
 
 
@@ -26,18 +28,19 @@ Adafruit_WS2801 strip = Adafruit_WS2801(15);
 
 
 void setup() {
-  
+
 
   strip.begin();
 
   // Update LED contents, to start they are all 'off'
   strip.show();
-  
-  
-  pinMode(buttonPin1,INPUT_PULLUP);
-  pinMode(buttonPin2,INPUT_PULLUP);
-  
+
+
+  pinMode(buttonPin1, INPUT_PULLUP);
+  pinMode(buttonPin2, INPUT_PULLUP);
+
   init_display();
+  now = millis();
 }
 
 
@@ -49,50 +52,87 @@ void setup() {
 
 
 void loop() {
-  
-  
 
- if(currentpos==15){ rainbowCycle(random(10,20)); return;}
- if(currentpos==-1){ redFade(); return;}
-  
 
-  
- // check if button was pressed
- if(!digitalRead(buttonPin1))
- {
-  if(currentpos==-1) return;
-  strip.setPixelColor(currentpos,Color(0,255,0));
-  currentpos--;
-  strip.setPixelColor(currentpos,Color(255,255,255));
-    strip.show();
-  while(!digitalRead(buttonPin1)){}
-  delay(10000);
-  
- }
- 
- if(!digitalRead(buttonPin2))
- {
-  if(currentpos==15) return;
-  strip.setPixelColor(currentpos,Color(255,0,0));
-  currentpos++;
-  strip.setPixelColor(currentpos,Color(255,255,255));
-    strip.show();
-    while(!digitalRead(buttonPin2)){}  
+
+  if (currentpos == 15) {
+    rainbowCycle(random(10, 20));
+    return;
+  }
+  if (currentpos == -1) {
+    redFade();
+    return;
+  }
+
+
+
+  // check if button was pressed
+  if (!digitalRead(buttonPin1))
+  {
+    if (currentpos == -1) return;
+    /*strip.setPixelColor(currentpos, Color(0, 255, 0));
+    currentpos--;
+    strip.setPixelColor(currentpos, Color(255, 255, 255));
+    strip.show();*/
+    mood_dn();
+    while (!digitalRead(buttonPin1)) {}
     delay(10000);
-  
- }  
-  
 
-  
-  
-  
+  }
+
+  if (!digitalRead(buttonPin2))
+  {
+    if (currentpos == 15) return;
+    /*strip.setPixelColor(currentpos, Color(255, 0, 0));
+    currentpos++;
+    strip.setPixelColor(currentpos, Color(255, 255, 255));
+    strip.show();*/
+    mood_up();
+    while (!digitalRead(buttonPin2)) {}
+    delay(10000);
+
+    now = millis();
+    if (tdelta() > expTime && currentpos != 7) {
+      t_prev = now;
+
+    }
+  }
+
   fadeLed(ledPin1);
   fadeLed(ledPin2);
-    
+
 }
 
+unsigned long tdelta () {
+  if (now > t_prev) {
+    return now - t_prev;
+  }
+  return sizeof(unsigned int) - t_prev + now;
+}
 
+void normalizeMood () {
+  if (currentpos > 7) {
+    mood_dn();
+  }
+  else if (currentpos < 7) {
+    mood_up();
+  }
 
+}
+
+void mood_up () {
+  strip.setPixelColor(currentpos, Color(255, 0, 0));
+  currentpos++;
+  strip.setPixelColor(currentpos, Color(255, 255, 255));
+  strip.show();
+}
+
+void mood_dn () {
+  strip.setPixelColor(currentpos, Color(0, 255, 0));
+  currentpos--;
+  strip.setPixelColor(currentpos, Color(255, 255, 255));
+  strip.show();
+}
 
 
 
@@ -102,33 +142,45 @@ void loop() {
 
 void redFade() {
 
-  
-    for (int j=50; j < 255; j++) 
-    {
-    for (int i=0; i < strip.numPixels(); i++) {
-      strip.setPixelColor(i, Color(0,j,0) );
-    }  
-    strip.show();   // write all the pixels out
-    if(bd(10)){ currentpos=0; resetRed(); return;}
+
+  for (int j = 50; j < 255; j++)
+  {
+    for (int i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Color(0, j, 0) );
     }
-    
-      if(bd(1000)){ currentpos=0; resetRed(); return;}
-    
- 
-   
-    for (int j=255; j >50 ; j--) 
-    {
-    for (int i=0; i < strip.numPixels(); i++) {
-      strip.setPixelColor(i, Color(0,j,0) );
-    }  
     strip.show();   // write all the pixels out
-    if(bd(10)){ currentpos=0; resetRed(); return;}
+    if (bd(10)) {
+      currentpos = 0;
+      resetRed();
+      return;
     }
- 
- 
- 
- 
- 
+  }
+
+  if (bd(1000)) {
+    currentpos = 0;
+    resetRed();
+    return;
+  }
+
+
+
+  for (int j = 255; j > 50 ; j--)
+  {
+    for (int i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Color(0, j, 0) );
+    }
+    strip.show();   // write all the pixels out
+    if (bd(10)) {
+      currentpos = 0;
+      resetRed();
+      return;
+    }
+  }
+
+
+
+
+
 }
 
 
@@ -136,27 +188,12 @@ void redFade() {
 
 void   init_display()
 {
-    strip.setPixelColor(14, Color(0,255,0));    
-    strip.setPixelColor(13, Color(0,255,0));   
-    strip.setPixelColor(12, Color(0,255,0)); 
-    strip.setPixelColor(11, Color(0,255,0)); 
-    strip.setPixelColor(10, Color(0,255,0)); 
-    strip.setPixelColor(9, Color(0,255,0)); 
-    strip.setPixelColor(8, Color(0,255,0)); 
+  for (int i = 0; i < strip.numPixels(); ++i) {
+    strip.setPixelColor(i, Color(0, 255, 0));
+  }
+  strip.setPixelColor(strip.numPixels()/2, Color(255, 255, 255));
 
-    strip.setPixelColor(7, Color(255,255,255)); 
-   
-    strip.setPixelColor(6, Color(255,0,0));    
-    strip.setPixelColor(5, Color(255,0,0));   
-    strip.setPixelColor(4, Color(255,0,0)); 
-    strip.setPixelColor(3, Color(255,0,0)); 
-    strip.setPixelColor(2, Color(255,0,0)); 
-    strip.setPixelColor(1, Color(255,0,0)); 
-    strip.setPixelColor(0, Color(255,0,0));     
-  
-    
-    strip.show();  
-  
+  strip.show();
 }
 
 
@@ -166,11 +203,11 @@ void   init_display()
 
 void resetGreen()
 {
-     for (int i=0; i < strip.numPixels(); i++) {
-      strip.setPixelColor(i,Color(255,0,0));
-    }  
-    strip.show();   // write all the pixels out  
-  
+  for (int i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, Color(255, 0, 0));
+  }
+  strip.show();   // write all the pixels out
+
 }
 
 
@@ -178,11 +215,11 @@ void resetGreen()
 
 void resetRed()
 {
-     for (int i=0; i < strip.numPixels(); i++) {
-      strip.setPixelColor(i,Color(0,255,0));
-    }  
-    strip.show();   // write all the pixels out  
-  
+  for (int i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, Color(0, 255, 0));
+  }
+  strip.show();   // write all the pixels out
+
 }
 
 
@@ -193,12 +230,12 @@ void resetRed()
 
 void fadeLed(int ledPin)
 {
-  
-    for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 5) {
-     // sets the value (range from 0 to 255):
+
+  for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 5) {
+    // sets the value (range from 0 to 255):
     analogWrite(ledPin, fadeValue);
     // wait for 30 milliseconds to see the dimming effect
-    if(bd(5)) return;
+    if (bd(5)) return;
   }
 
   // fade out from max to min in increments of 5 points:
@@ -206,9 +243,9 @@ void fadeLed(int ledPin)
     // sets the value (range from 0 to 255):
     analogWrite(ledPin, fadeValue);
     // wait for 30 milliseconds to see the dimming effect
-    if(bd(5)) return;
-  } 
-  
+    if (bd(5)) return;
+  }
+
 }
 
 
@@ -217,35 +254,39 @@ void fadeLed(int ledPin)
 
 int bd(int del)
 {
- for(int i =0;i<del;i++)
- {
-   
-  delay(1);
-  if((!digitalRead(buttonPin2))  || (!digitalRead(buttonPin1))) return 1;
- } 
- return 0; 
-  
-  
+  for (int i = 0; i < del; i++)
+  {
+
+    delay(1);
+    if ((!digitalRead(buttonPin2))  || (!digitalRead(buttonPin1))) return 1;
+  }
+  return 0;
+
+
 }
 
 
 
 
-// Slightly different, this one makes the rainbow wheel equally distributed 
+// Slightly different, this one makes the rainbow wheel equally distributed
 // along the chain
 void rainbowCycle(uint8_t wait) {
   int i, j;
-  
-  for (j=0; j < 256 * 5; j++) {     // 5 cycles of all 25 colors in the wheel
-    for (i=0; i < strip.numPixels(); i++) {
+
+  for (j = 0; j < 256 * 5; j++) {   // 5 cycles of all 25 colors in the wheel
+    for (i = 0; i < strip.numPixels(); i++) {
       // tricky math! we use each pixel as a fraction of the full 96-color wheel
       // (thats the i / strip.numPixels() part)
       // Then add in j which makes the colors go around per pixel
       // the % 96 is to make the wheel cycle around
       strip.setPixelColor(i, Wheel( ((i * 256 / strip.numPixels()) + j) % 256) );
-    }  
+    }
     strip.show();   // write all the pixels out
-    if(bd(wait)){ currentpos=14; resetGreen(); return;}
+    if (bd(wait)) {
+      currentpos = 14;
+      resetGreen();
+      return;
+    }
   }
 }
 
@@ -253,11 +294,11 @@ void rainbowCycle(uint8_t wait) {
 // good for testing purposes
 void colorWipe(uint32_t c, uint8_t wait) {
   int i;
-  
-  for (i=0; i < strip.numPixels(); i++) {
-      strip.setPixelColor(i, c);
-      strip.show();
-      delay(wait);
+
+  for (i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
   }
 }
 
@@ -280,12 +321,12 @@ uint32_t Color(byte r, byte g, byte b)
 uint32_t Wheel(byte WheelPos)
 {
   if (WheelPos < 85) {
-   return Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+    return Color(WheelPos * 3, 255 - WheelPos * 3, 0);
   } else if (WheelPos < 170) {
-   WheelPos -= 85;
-   return Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    WheelPos -= 85;
+    return Color(255 - WheelPos * 3, 0, WheelPos * 3);
   } else {
-   WheelPos -= 170; 
-   return Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    WheelPos -= 170;
+    return Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
 }
